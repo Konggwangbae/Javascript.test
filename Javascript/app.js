a60f90be81ac30276a803f104358ae50b26bf89b
Konggwangbae <109287581+Konggwangbae@users.noreply.github.com>
2023-12-02 21:10:51 +0900

    Add files via upload

---
+++ Javascript/app.js
@@ -0,0 +1,8 @@
+let age = 33;
+let userNmae = 'Hardy';
+let hobbies = ['Sports', 'Cooking', 'Reading'];
+
+alert(age);
+alert(hobbies[1]);
+// [0]은 Sports, [1]은 Cooking, [2]는 Reading으로 인식해서 출력한다.
+// alert은 출력 값
